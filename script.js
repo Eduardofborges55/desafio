750a1360edb67e839f314afb17dc665e83013872
Eduardofborges55 <borgeseduardof55@gmail.com>
2025-03-28 21:50:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function soma(a,b){
+    return a+b
+}
+
+const results = soma(2,20)
+console.log(results)
+function multi(a,b,c,d){
+    // return soma(a,b)
+    return a+b+c+d
+    
+}
+const resultm = soma(4,4,4,4)
+console.log(resultm)
+function divide(){
+    return Math.floor(4.2)
+}
+const resultd = divide;
+console.log(resultd);
+
+soma()
